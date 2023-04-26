38bd5dfbbd64153fc13c71a38093d7dc551ade3a
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-26 22:11:54 +0800

    Import pyfiglet and set and print the title of the activity

--- file_handling_integers.py
+++ file_handling_integers.py
@@ -1,3 +1,12 @@
+# import pyfiglet
+import pyfiglet
+
+# Set and print the title of the activity in color and different font
+title_of_assign = "Module 2-Prob 1"
+font = "slant"
+print("\033[38;5;206m", pyfiglet.figlet_format(title_of_assign, font=font))
+
+
 # a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
 
 
