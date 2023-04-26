dd3e8bfc8a2f6c1a46b8b1534a8342055e65f5d2
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-26 21:54:46 +0800

    Pseudocode

---
+++ file_handling_integers.py
@@ -0,0 +1,23 @@
+# a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+
+# Start
+
+# Create a function called process
+
+    # open numbers.txt (read), even.txt (write), odd.txt (write)
+
+        # Loop through each line in numbers.txt
+
+            # Convert the line into integer 
+
+            # Print number to the console to check if the code is working
+
+            # Check if the number is even or odd
+
+                # If even, write it to even.txt
+
+                # If odd, write it to odd.txt
+
+
+# Execute the function
